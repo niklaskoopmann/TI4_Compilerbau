316f3120ffa82bb8da8d93836e5043d19e55612a
Niklas Koopmann <koopmann.niklas@gmail.com>
2019-02-07 12:37:52 +0100

    started implementing tdp

--- src/Parser/TopDownParser.java
+++ src/Parser/TopDownParser.java
@@ -1,4 +1,43 @@
 package Parser;
 
+import SyntaxTree.BinOpNode;
+import SyntaxTree.OperandNode;
+import SyntaxTree.SyntaxNode;
+
 public class TopDownParser {
+
+    // Attributes
+    private String regex;
+
+    // Constructor
+    public TopDownParser(String regex) {
+        this.regex = regex;
+    }
+
+    // Getter methods
+    public String getRegex() {
+        return regex;
+    }
+
+    // Setter methods
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+    // logic
+    private SyntaxNode start(char c){
+
+        if(c == '#') return new OperandNode("#");
+
+        else if(c == '('){
+
+            OperandNode leaf = new OperandNode("#");
+
+            return new BinOpNode("°", regExp(null), leaf);
+        }
+
+        else return null;
+    }
+
+    private SyntaxNode regExp();
 }
